5bfe9e36d5db4896a8820abd2e854009f86d9355
alejandroberod <luisbetancourth43@gmail.com>
2025-10-03 21:46:26 -0500

    Customizing the form

--- app/components/signForm.tsx
+++ app/components/signForm.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,65 +10,110 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .min(2, { error: "Username must be at least 2 characters." })
+    .max(50, { error: "Username must be under 50 characters." }),
+});
 
 export default function SignForm({ type }) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
     <div className={cn("flex flex-col gap-6")}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-center text-[#23C2ED] text-[24px]">PrepWise</CardTitle>
+          <CardTitle className="text-center text-[#23C2ED] text-[24px]">
+            PrepWise
+          </CardTitle>
           <CardDescription className="text-[20px] text-center text-white font-light">
             Interviews powered by AI
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-3">
-                <Label htmlFor="text">Name</Label>
-                <Input
-                  id="text"
-                  type="text"
-                  placeholder="Enter your full name"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter your full name"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* <Label htmlFor="text">Name</Label>
+                  <Input
+                    id="text"
+                    type="text"
+                    placeholder="Enter your full name"
+                    required
+                  /> */}
                 </div>
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="cv">Resume</Label>
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input id="email" type="email" placeholder="m@example.com" />
                 </div>
-                <Input id="cv" type="file" required className="hidden"/>
-                <button className="w-full bg-transparent text-[#666666] h-12">Upload a pdf</button>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="Enter your password"
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="cv">Resume</Label>
+                  </div>
+                  <Input id="cv" type="file" className="hidden" />
+                  <button className="w-full bg-transparent text-[#666666] h-12">
+                    Upload a pdf
+                  </button>
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button type="submit" className="w-full">
+                    Login
+                  </Button>
+                </div>
               </div>
-              <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
-                  Login
-                </Button>
-              </div>
-            </div>
-          </form>
+            </form>
+          </Form>
         </CardContent>
       </Card>
     </div>
