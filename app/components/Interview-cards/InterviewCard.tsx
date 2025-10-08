6fccaee82a73ab2936b03494979181620abfbf70
Alejandro Betancourth <luisbetancourth43@gmail.com>
2025-10-08 09:43:14 -0500

    Advances in interviewCards component

--- app/components/Interview-cards/InterviewCard.tsx
+++ app/components/Interview-cards/InterviewCard.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import dayjs from "dayjs";
-import { getRandomInterviewCover } from "@/public/utils";
+import { getRandomInterviewCover } from "@/lib/utils";
 import Link from "next/link";
+import TechIcons from "./TechIcons";
 
 export default function InterviewCard({
   interviewId,
@@ -34,7 +35,7 @@ export default function InterviewCard({
             "You haven taken this interview yet. Take it now to improve your skills"}
         </p>
       </header>
-      <div className="flex flex-row gap-5 mt-3">
+      {/* <div className="flex flex-row gap-5 mt-3">
         <div className="flex flex-row gap-2">
           <Image
             src="/calendar-add.svg"
@@ -47,17 +48,13 @@ export default function InterviewCard({
         <div className="flex flex-row gap-2 items-center">
           <Image src="/star-.svg" alt="star" width={22} height={22} />
           <p className="font-light">{feedback?.totalScore || "---"}/100</p>{" "}
-          {/*feedback?.totalScore || '---'  */}
         </div>
-      </div>
+      </div> */}
       <div className="border py-2 px-3 w-fit bg-[#1A1A1A] rounded-3xl">
         <span className="text-white text-md">{normalizedType}</span>
       </div>
-      <footer className="flex justify-between">
-        <div className="flex">
-          <Image src="/react.svg" alt="logo" width={25} height={25} />
-          <Image src="/tailwind.svg" alt="logo" width={25} height={25} />
-        </div>
+      <footer className="flex justify-between items-center gap-2">
+        <TechIcons techStack={techstack} />
         <Button className="w-3/5 lg:text-lg lg:py-6 xl:py-8">
           <Link
             href={
@@ -66,7 +63,7 @@ export default function InterviewCard({
                 : `/interview/${interviewId}`
             }
           >
-            {feedback ? 'Feedback' : 'View Interview'}
+            {feedback ? "Feedback" : "View Interview"}
           </Link>
         </Button>
       </footer>
