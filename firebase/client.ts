ffeaff90e96b2c6525f744a00e2bd3b0b3fb6fa7
alejandroberod <luisbetancourth43@gmail.com>
2025-10-10 19:43:31 -0500

    credentials hidden

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBuaPmC0whSPZWaXnrrUlM_QliBbEdxNh0",
-  authDomain: "intervai-5d06f.firebaseapp.com",
-  projectId: "intervai-5d06f",
-  storageBucket: "intervai-5d06f.firebasestorage.app",
-  messagingSenderId: "49644075845",
-  appId: "1:49644075845:web:2d1e5dfe97d9a83d525c2c",
-  measurementId: "G-NKMQ2VY1LV",
+  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
